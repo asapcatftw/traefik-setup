7d0da0ce233f4bd4da2c23154526aea4507e7f60
asapcatftw <31966895+asapcatftw@users.noreply.github.com>
2019-08-30 17:44:53 -0400

    added wget urls

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ mkdir -p /opt/traefik
 touch /opt/traefik/docker-compose.yml
 touch /opt/traefik/acme.json && chmod 600 /opt/traefik/acme.json
 touch /opt/traefik/traefik.toml
+wget https://github.com/asapcatftw/traefik-setup/blob/master/docker-compose.yml
+wget https://github.com/asapcatftw/traefik-setup/blob/master/traefik.toml
+echo "Add your domain and email under [docker] and [acme], in traefik.toml"
