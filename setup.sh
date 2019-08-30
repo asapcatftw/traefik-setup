902c44e0618b69eca4d11b89c3799fa72591c137
asapcatftw <31966895+asapcatftw@users.noreply.github.com>
2019-08-30 17:35:02 -0400

    setup.sh added

---
+++ setup.sh
@@ -0,0 +1,6 @@
+docker network create web
+sudo su
+mkdir -p /opt/traefik
+touch /opt/traefik/docker-compose.yml
+touch /opt/traefik/acme.json && chmod 600 /opt/traefik/acme.json
+touch /opt/traefik/traefik.toml
