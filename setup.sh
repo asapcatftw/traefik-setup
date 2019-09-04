c025f517fcae6166a680b59c48d5704deb131151
asapcatftw <31966895+asapcatftw@users.noreply.github.com>
2019-09-04 09:20:21 -0400

    Updated Setup

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 docker network create web
 sudo su
 mkdir -p /opt/traefik
-touch /opt/traefik/docker-compose.yml
-touch /opt/traefik/acme.json && chmod 600 /opt/traefik/acme.json
-touch /opt/traefik/traefik.toml
-wget https://github.com/asapcatftw/traefik-setup/blob/master/docker-compose.yml
-wget https://github.com/asapcatftw/traefik-setup/blob/master/traefik.toml
+cd /opt/traefik
+wget https://raw.githubusercontent.com/asapcatftw/traefik-setup/master/docker-compose.yml
+wget https://raw.githubusercontent.com/asapcatftw/traefik-setup/master/traefik.toml
+chmod 600 /opt/traefik/acme.json
 echo "Add your domain and email under [docker] and [acme], in traefik.toml"
