5746f023af852b483fa4ebd90340d3f36ae9e795
asapcatftw <31966895+asapcatftw@users.noreply.github.com>
2019-09-17 20:05:41 -0400

    added acme file

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ mkdir -p /opt/traefik
 cd /opt/traefik
 wget https://raw.githubusercontent.com/asapcatftw/traefik-setup/master/docker-compose.yml
 wget https://raw.githubusercontent.com/asapcatftw/traefik-setup/master/traefik.toml
+touch /opt/traefik/acme.json
 chmod 600 /opt/traefik/acme.json
 echo "Add your domain and email under [docker] and [acme], in traefik.toml"
